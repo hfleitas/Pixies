9a5c21fc060ecbdbe5223265fd4454020f98d48c
Hiram Fleitas <hiramfleitas@hotmail.com>
2018-01-24 23:10:41 -0500

    sysmail_failedsentitems

--- ConsoleApp1/AmuletOfDaylight.sql
+++ ConsoleApp1/AmuletOfDaylight.sql
@@ -98,7 +98,8 @@ begin
 
 	exec msdb.dbo.sp_send_dbmail @recipients ='hf0524@universalproperty.com' --input your own email!
 		,@subject = 'Trolls are attacking' --Alert: Divide by zero attack.
-		,@body = @body; --select * from msdb.dbo.sysmail_faileditems --sentitems, unsentitems, 
+		,@body = @body; 
+		--select * from msdb.dbo.sysmail_faileditems --sentitems, unsentitems
 end
 else begin print 'Coast is clear.' end
 go
